248e80619e888d4d4d92d7bb1685869ae62232ea
Christopher Schymura <christopher.schymura@rub.de>
2016-12-07 16:51:32 +0100

    Removed license information from sampleVonMises.m.

--- @VonMisesMixture/private/sampleVonMises.m
+++ @VonMisesMixture/private/sampleVonMises.m
@@ -20,26 +20,12 @@ function angles = sampleVonMises(mu, kappa, varargin)
 %       Ratio-of-Uniforms Method"
 %
 % AUTHOR:
-%   Copyright (c) 2015      Christopher Schymura
+%   Copyright (c) 2016      Christopher Schymura
 %                           Cognitive Signal Processing Group
 %                           Ruhr-Universitaet Bochum
 %                           Universitaetsstr. 150
 %                           44801 Bochum, Germany
 %                           E-Mail: christopher.schymura@rub.de
-%
-% LICENSE INFORMATION:
-%   This program is free software: you can redistribute it and/or
-%   modify it under the terms of the GNU General Public License as
-%   published by the Free Software Foundation, either version 3 of the
-%   License, or (at your option) any later version.
-%
-%   This material is distributed in the hope that it will be useful,
-%   but WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-%   GNU General Public License for more details.
-%
-%   You should have received a copy of the GNU General Public License
-%   along with this program. If not, see <http://www.gnu.org/licenses/>
 
 % Check inputs
 p = inputParser();
